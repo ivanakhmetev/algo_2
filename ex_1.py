dc510dc989c4720ca75f245ef3a3b830c6eaeac3
Master <ivanakhmetev@gmail.com>
2024-02-03 15:58:42 +0300

    del node iscl 5

--- ex_1.py
+++ ex_1.py
@@ -22,7 +22,7 @@ class SimpleTree:
         parent = NodeToDelete.Parent
         if parent:
             NodeToDelete.Parent.Children.remove(NodeToDelete)
-        NodeToDelete = None
+        del NodeToDelete
         # pass # ваш код удаления существующего узла NodeToDelete
 
     def GetAllNodes(self):

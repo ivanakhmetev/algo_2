5c049247f333b36fec9557c84f89750d5d9a57f0
Master <ivanakhmetev@gmail.com>
2024-02-04 17:03:06 +0300

    iscl new

--- ex_1.py
+++ ex_1.py
@@ -14,6 +14,7 @@ class SimpleTree:
 	
     def AddChild(self, ParentNode, NewChild):
         ParentNode.Children.append(NewChild)
+        NewChild.Parent = ParentNode
 
     def DeleteNode(self, NodeToDelete):
         if NodeToDelete == self.Root:

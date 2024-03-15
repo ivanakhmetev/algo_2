6347529533828237ff748908b74b4a5475ca36c5
ivanakhmetev <78900410+ivanakhmetev@users.noreply.github.com>
2024-03-15 10:42:52 +0300

    Update ex_10.py

--- ex_10.py
+++ ex_10.py
@@ -53,18 +53,58 @@ class SimpleGraph:
             return self.vertex.index(v)
         
     def DepthFirstSearch(self, VFrom, VTo):
-        stack = Stack()
+        self.stack = Stack()
         self.SetUnhit()
-        if self._DepthFirstSearch(VFrom, VTo, stack) is True:
-            return stack
-        for i in range(len(self.m_adjacency[VFrom])): #4
-            if self.m_adjacency[i] == 1 and self.vertex[i].Hit == False:
-                if self._DepthFirstSearch(i, VTo, stack) is True:
-                    return stack
-        stack.pop()
-        if stack.len() == 0:
-            return []
-        for i 
+        self.step_1(VFrom, VTo)
+        return self.stack.stack
+
+
+    def step_1(self, current, VTo):
+        self.vertex[current].Hit = True
+        self.stack.push(current)
+        adjacency = self.m_adjacency[current]
+        # print('stack',self.stack.stack)
+        # print('curr adj', adjacency)
+        self.step_4(adjacency, VTo)
+        
+
+
+
+    def step_4(self, adjacency, VTo):
+        if adjacency[VTo] == 1:
+            self.stack.push(VTo)
+            # print(self.stack.stack)
+            return
+            # return self.stack
+        if adjacency[VTo] == 0:
+            unvisit_neighbours = [i for i in range(len(adjacency)) if self.vertex[i].Hit is False and adjacency[i] == 1]
+            # print('unvisit', unvisit_neighbours)
+            # print(unvisit_neighbours)
+            if len(unvisit_neighbours) != 0:
+                self.step_1(unvisit_neighbours[0], VTo)
+            if len(unvisit_neighbours) == 0:
+                # print('len')
+                self.stack.pop()
+                if len(self.stack) == 0:
+                    return []
+                if len(self.stack) != 0:
+                    new_current = self.stack.pop()
+                    self.step_1(new_current, VTo)
+
+
+        # self._DepthFirstSearch(VFrom, VTo, stack)
+
+
+        # if self._DepthFirstSearch(VFrom, VTo, stack) is True:
+        #     return stack
+        # for i in range(len(self.m_adjacency[VFrom])): #4
+        #     if self.m_adjacency[i] == 1 and self.vertex[i].Hit == False:
+        #         if self._DepthFirstSearch(i, VTo, stack) is True:
+        #             return stack
+        # stack.pop()
+        # if stack.len() == 0:
+        #     return []
+        # for i 
                 
 
         # current = self.vertex[VFrom]
@@ -75,28 +115,30 @@ class SimpleGraph:
         #     if collumn[i] == 1 and i == VTo:
         #         stack.push(VTo)
         #         return stack
-        for i in range(len(collumn)):
-            if collumn[i] == 1 and self.vertex[i].Hit == False:
-                current = self.vertex[i]
+    #     for i in range(len(collumn)):
+    #         if collumn[i] == 1 and self.vertex[i].Hit == False:
+    #             current = self.vertex[i]
         
-    def _DepthFirstSearch(self, current, VTo, stack):
-        stack.push(current)
-        collumn = self.m_adjacency[current]
-        for i in range(len(collumn)):
-            if collumn[i] == 1 and i == VTo:
-                stack.push(VTo)
-                return True
-        return False
+    # def _DepthFirstSearch(self, current, VTo, stack):
+    #     self.vertex[current].Hit = True
+    #     stack.push(current)
+    #     neighbours = self.m_adjacency[current]
+    #     if neighbours[VTo] == 1:
+    #         stack.push(VTo)
+    #         return
+    #     stack.pop()
+    #     return
+    
+        # for i in range(len(collumn)):
+        #     if collumn[i] == 1 and i == VTo:
+        #         stack.push(VTo)
+        #         return True
+        # return False
 
 
         
 
 
-
-        # узлы задаются позициями в списке vertex
-        # возвращается список узлов -- путь из VFrom в VTo
-        # или [] если пути нету
-        pass
 
     def SetUnhit(self):
         for el in self.vertex:
